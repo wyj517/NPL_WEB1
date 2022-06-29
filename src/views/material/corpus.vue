3d76e375903b988b341ad98483e0c75b82d3bf6c
ssssyy <578103878@qq.com>
2022-06-29 16:07:25 +0800

    feat:语料管理页面

--- src/views/material/corpus.vue
+++ src/views/material/corpus.vue
@@ -43,7 +43,7 @@
         <el-table-column prop="manual_tag" label="标签" width="150">
           <template slot-scope="scope">
             <el-select v-model="scope.row.tag" placeholder="请选择" filterable allow-create
-              @change="updateClassTag(0, scope.row.id, scope.row.manual_tag)">
+              @change="updateClassTag(0, scope.row.id, scope.row.tag)">
               <el-option v-for="(item, index) in TagOptions" :key="index" :label="item" :value="item" />
             </el-select>
           </template>
@@ -95,7 +95,7 @@
 import { getData,getManual,update } from "@/api/corpus";
 
 import excel from "@/utils/excel";
-let staticSel = false;
+
 export default {
   name: "Index",
   components: {},
@@ -143,12 +143,12 @@ export default {
   },
   methods: {
     // 分析
-    toAnalysis() {
-      this.$router.push({
-        name: "Analysis",
-        query: { id: this.$route.query.dataset_id },
-      });
-    },
+    // toAnalysis() {
+    //   this.$router.push({
+    //     name: "Analysis",
+    //     query: { id: this.$route.query.dataset_id },
+    //   });
+    // },
     handleSelectionChange(val) {
       this.multipleSelection = val;
       let ids = [];
@@ -170,13 +170,15 @@ export default {
         this.$message.success('标签添加成功')
       }
     },
-    toggleSelection(rows) {
-      staticSel = !staticSel;
-      for (let i = 0; i < rows.length; i++) {
-        this.$refs.multipleTable.toggleRowSelection(rows[i], staticSel);
-      }
-      this.is_total = staticSel;
-    },
+
+    // toggleSelection(rows) {
+    //   staticSel = !staticSel;
+    //   for (let i = 0; i < rows.length; i++) {
+    //     this.$refs.multipleTable.toggleRowSelection(rows[i], staticSel);
+    //   }
+    //   this.is_total = staticSel;
+    // },
+
     handleSizeChange(val) {
       this.page.currentPage = 1;
       this.page.pageSize = val;
@@ -208,38 +210,38 @@ export default {
     },
 
     // 导出
-    async exportExcel() {
-      this.exportLoading = true;
-      let params = {
-        page: 1,
-        page_size: 999999,
-        total_flg: true,
-        task_id: this.$route.query.id,
-        dataset_id: this.$route.query.dataset_id,
-        class_id: this.Typevalue,
-        manual_tag: this.manual_tag,
-        doc: this.findContent,
-      };
-      let res = await getTaskResult(params);
-      this.exportExcelFu(res.data);
-    },
-    // 导出参数
-    exportExcelFu(tableData) {
-      if (tableData) {
-        const params = {
-          title: ["数据ID", "内容", "类型编号", "自动标签", "最终标签"],
-          key: ["id", "doc", "class_id", "predict_tag", "manual_tag"],
-          data: tableData,
-          autoWidth: true,
-          filename: "分类列表",
-        };
-        excel.export_array_to_excel(params);
-        this.exportLoading = false;
-      } else {
-        console.error("表格数据不能为空！");
-        // this.$Message.success('表格数据不能为空！')
-      }
-    },
+    // async exportExcel() {
+    //   this.exportLoading = true;
+    //   let params = {
+    //     page: 1,
+    //     page_size: 999999,
+    //     total_flg: true,
+    //     task_id: this.$route.query.id,
+    //     dataset_id: this.$route.query.dataset_id,
+    //     class_id: this.Typevalue,
+    //     manual_tag: this.manual_tag,
+    //     doc: this.findContent,
+    //   };
+    //   let res = await getTaskResult(params);
+    //   this.exportExcelFu(res.data);
+    // },
+    // // 导出参数
+    // exportExcelFu(tableData) {
+    //   if (tableData) {
+    //     const params = {
+    //       title: ["数据ID", "内容", "类型编号", "自动标签", "最终标签"],
+    //       key: ["id", "doc", "class_id", "predict_tag", "manual_tag"],
+    //       data: tableData,
+    //       autoWidth: true,
+    //       filename: "分类列表",
+    //     };
+    //     excel.export_array_to_excel(params);
+    //     this.exportLoading = false;
+    //   } else {
+    //     console.error("表格数据不能为空！");
+    //     // this.$Message.success('表格数据不能为空！')
+    //   }
+    // },
 
 
 
@@ -253,24 +255,49 @@ export default {
     //批量更新数据标签
     updateClassTag(flg, id, tag) {
       //flg 1批量 0单改
-      if (tag || this.Tagvalue) {
-        let params = {
-          label_ids: flg ? this.selectedIDs : [id],
-          manual_tag: tag,
-          is_total: this.is_total,
-          tag: "",
-          doc: "",
-        };
-        update(params).then((res) => {
-          if (res.success) {
-            this.$message.success(res.msg);
-            // this.addTag(tag);
-            this.dialogVisible = false;
-            this.getResult();
-          }
-        });
-      } else {
-        this.$message.error("请先选择标签");
+
+      if (flg){
+        if (this.selectedIDs.length){
+            let params = {
+              label_ids: flg ? this.selectedIDs : [id],
+              manual_tag: tag,
+              is_total: this.is_total,
+              tag: "",
+              doc: "",
+            };
+            console.log('params',params)
+            update(params).then((res) => {
+              if (res.success) {
+                this.$message.success(res.msg);
+                // this.addTag(tag);
+                this.dialogVisible = false;
+                this.getResult();
+              }
+            });
+        }else {
+          this.$message.error("请先选择标签");
+        }
+      }else {
+        if (tag || this.Tagvalue) {
+          let params = {
+            label_ids: flg ? this.selectedIDs : [id],
+            manual_tag: tag,
+            is_total: this.is_total,
+            tag: "",
+            doc: "",
+          };
+          console.log('params',params)
+          update(params).then((res) => {
+            if (res.success) {
+              this.$message.success(res.msg);
+              // this.addTag(tag);
+              this.dialogVisible = false;
+              this.getResult();
+            }
+          });
+        } else {
+          this.$message.error("请先选择标签");
+        }
       }
     },
     //重新分析
